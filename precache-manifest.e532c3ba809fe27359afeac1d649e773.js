5092cb8356a20778e5885dde8ab49419f621a8c2
poojapower <54731691+poojapower@users.noreply.github.com>
2020-07-20 12:50:11 -0700

    Updates

---
+++ precache-manifest.e532c3ba809fe27359afeac1d649e773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2223d6de40c307249ae856f2f7efb2",
+    "url": "/HangmanGames/index.html"
+  },
+  {
+    "revision": "d597a52ebdf58b98c392",
+    "url": "/HangmanGames/static/css/main.341a48cd.chunk.css"
+  },
+  {
+    "revision": "d4b90e2067b45528a8d4",
+    "url": "/HangmanGames/static/js/2.90c0c040.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HangmanGames/static/js/2.90c0c040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d597a52ebdf58b98c392",
+    "url": "/HangmanGames/static/js/main.735e12ca.chunk.js"
+  },
+  {
+    "revision": "c3cc53c934027d60d21d",
+    "url": "/HangmanGames/static/js/runtime-main.a037280c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HangmanGames/static/media/logo.5d5d9eef.svg"
+  }
+]);
