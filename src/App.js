d2603c9baacf8e9b4e859b3da5b726a457c2d478
poojapower <54731691+poojapower@users.noreply.github.com>
2020-07-20 13:06:35 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Hangman from './components/Hangman'
 
 function App() {
   return (
     <div className="App">
-     
+     Learn React
       <Hangman/>
     </div>
   );
